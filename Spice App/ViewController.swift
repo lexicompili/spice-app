3d06061739c9c161fd563763b1a7615c11d9626a
Mary Alexis Solis <lexi@Marys-MacBook-Pro.local>
2015-07-25 16:49:45 +0800

    Initial Commit

---
+++ Spice App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Spice App
+//
+//  Created by Mary Alexis Solis on 7/25/15.
+//  Copyright (c) 2015 Mary Alexis Solis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
