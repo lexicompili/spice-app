183c015b6ef1288a1d4dfbcfedb2613d44be4fe8
Mary Alexis Solis <lexi@Marys-MacBook-Pro.local>
2015-07-28 09:08:57 +0800

    Added home view files

---
+++ Spice App/Views/SAHomeView.swift
@@ -0,0 +1,21 @@
+//
+//  SAHomeView.swift
+//  Spice App
+//
+//  Created by Mary Alexis Solis on 7/25/15.
+//  Copyright (c) 2015 Mary Alexis Solis. All rights reserved.
+//
+
+import UIKit
+
+class SAHomeView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
